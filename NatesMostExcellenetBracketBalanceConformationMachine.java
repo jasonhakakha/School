0f8992d8e3ff4453361439efcd571c29b7a3f4f0
jasonhakakha <31663697+jasonhakakha@users.noreply.github.com>
2017-10-11 16:52:37 -0700

    Create NatesMostExcellenetBracketBalanceConformationMachine.java

---
+++ NatesMostExcellenetBracketBalanceConformationMachine.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class NatesMostExcellenetBracketBalanceConformationMachine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(bb("{[()]}"));
+		System.out.println(bb("{[(])}"));
+		System.out.println(bb("jew"));
+	}
+	static boolean bb(String sst){
+		Stack<Character> st = new Stack<Character>();
+		for(char x: sst.toCharArray()){	
+			if(x  == '(' || x == '[' || x == '{')
+				st.push(x);
+			else if (st.empty()) return false;
+			else{
+				char before = (char)st.pop();
+				if((before == '(' && x != ')')||(before == '[' && x != ']')||(before == '{' && x != '}'))
+					return false;
+			}
+			
+		}
+		
+		return true;
+	}
+
+}
